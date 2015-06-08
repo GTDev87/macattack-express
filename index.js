32c031c9f1e4855f0c7140cf9e13b4164d7e0088
Gregory Thompson <GT@Gregorys-MacBook-Pro-2.local>
2015-06-08 09:30:01 -0400

    added next call with error to express middleware

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 var macattack = require("../macattack");
 
+function getTokenFromReq(req, headerKey) {
+  if (req.headers && req.headers.authorization) {
+    var parts = req.headers.authorization.split(' ');
+    if (parts.length === 2 && parts[0] === headerKey) {
+      return parts[1];
+    }
+  }
+  throw new Error("macaroon not found");
+}
+
 module.exports = function (optionsObj) {
   var options = optionsObj || {};
+
+  console.log("options set for macattack express");
   return function (req, res, next){
+    debugger
 
-    if(macattack.validateMac()) { return next(new Error("Macaroon is not valid ")); }
+    console.log("running pre request");
 
+    var headerKey = optionsObj.headerKey || 'Bearer';
+
+    var serializedMac;
+    var route = req.path;
+    var databaseSecret = optionsObj.secret || "secret";
+    var requestData = req.body;
+
+    try {
+      serializedMac = getTokenFromReq(req, headerKey);
+    }catch (e) {
+      return next(e);
+    }
+
+    if(!macattack.validateMac(serializedMac, route, databaseSecret, requestData)) { return next(new Error("Macaroon is not valid ")); }
     return next();
   }
 };
