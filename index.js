b16b91b91bbba97f888c2ee1f86f86edc82244be
Gregory Thompson <gregt87@gmail.com>
2015-11-09 14:30:24 -0500

    updated macattack-express to use asnychronous creation

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var macattack = require("macattack"),
   macaroons = require("macaroons.js"),
   MacaroonsBuilder = macaroons.MacaroonsBuilder,
   MacaroonsVerifier = macaroons.MacaroonsVerifier,
-  cert_encoder = require("cert_encoder");
+  cert_encoder = require("cert_encoder")
+  pem = require("pem"),
+  crypto = require('crypto'),
+  publicKeyMacaroons = require('public-key-macaroons'),
+  https = require("https");
 
 function getTokenFromReq(req, headerKey) {
   if (req.headers && req.headers.authorization) {
@@ -12,28 +16,77 @@ function getTokenFromReq(req, headerKey) {
   throw new Error("macaroon not found");
 }
 
-module.exports = function (optionsObj) {
+function condenseCertificate(certVar){
+  return certVar
+    .replace("-----BEGIN CERTIFICATE-----", "")
+    .replace("-----END CERTIFICATE-----", "")
+    .replace(/\n/g, "");
+}
+
+module.exports = function (optionsObj, callback) {
   var options = optionsObj || {};
-  return function (req, res, next){
-    var serializedMacs;
-    // TODO LATER 
-    var condensedCert = cert_encoder.convert(req.connection.getPeerCertificate().raw)
-      .replace(/\n/g, "")
-      .replace("-----BEGIN CERTIFICATE-----", "")
-      .replace("-----END CERTIFICATE-----", "");//get rid of newlines and header and footer.
+  options.secret = optionsObj.secret || "secret";
+  options.hostPort = optionsObj.hostPort || "443";
+  options.hostIp = optionsObj.hostIp || "127.0.0.1";
+  options.cert = optionsObj.cert || "cert";
 
-    try { serializedMacs = getTokenFromReq(req, optionsObj.headerKey || 'Bearer'); }
-    catch (e) { return next(e); }
+  //This is the initializing portion
+  ////////
 
-    var eachMac = serializedMacs.split(",");
-    var macs = eachMac.map(function (serialMac) { return serialMac && MacaroonsBuilder.deserialize(serialMac); })
+  var serializedMacaroon = macattack.createMac(optionsObj.hostIp, optionsObj.hostPort, optionsObj.secret);
 
-    var rootMac = macs[0];    var dischargeMac = macs[1];
-    var requestReadyMac = dischargeMac && MacaroonsBuilder.modify(rootMac).prepare_for_request(dischargeMac).getMacaroon();
-    var rootMacVerifier = new MacaroonsVerifier(rootMac);
-    rootMacVerifier = rootMacVerifier.satisfyExact("cert = " + condensedCert);
-    rootMacVerifier = (requestReadyMac ? rootMacVerifier.satisfy3rdParty(requestReadyMac) : rootMacVerifier);
+  pem.getPublicKey(optionsObj.cert, function (err, data) {
+    if(err) {return callback(err);}
 
-    return rootMacVerifier.isValid(optionsObj.secret || "secret") ? next() : next(new Error("Macaroon is not valid "));
-  }
+    var caveatKey = crypto.createHash('md5').digest('hex');
+    console.log("cert = %j", optionsObj.cert);
+
+    var caveatMacaroon = publicKeyMacaroons.addPublicKey3rdPartyCaveat(serializedMacaroon, "Macattack", caveatKey, "cert = " + condenseCertificate(optionsObj.cert), data.publicKey);
+  
+    console.log("client_macaroon=" + JSON.stringify(caveatMacaroon));
+
+    //macattack_express
+    pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
+      if(err) {return callback(err);}
+      var options = {
+        key: keys.serviceKey, //do i need to save this off?
+        cert: keys.certificate,
+       
+        // This is necessary only if using the client certificate authentication.
+        // Without this some clients don't bother sending certificates at all, some do
+        requestCert: true,
+       
+        // Do we reject anyone who certs who haven't been signed by our recognised certificate authorities
+        rejectUnauthorized: false
+       
+        // This is necessary only if the client uses the self-signed certificate and you care about implicit authorization
+        //ca: [ fs.readFileSync('client/client-certificate.pem') ]//TODO how do i get rid of this
+      };
+
+      // Return Express server instance vial callback
+
+      return callback(null, function (req, res, next){
+        var serializedMacs;
+        // TODO LATER 
+        var condensedCert = condenseCertificate(cert_encoder.convert(req.connection.getPeerCertificate().raw));//get rid of newlines and header and footer.
+
+        try { serializedMacs = getTokenFromReq(req, optionsObj.headerKey || 'Bearer'); }
+        catch (e) { return next(e); }
+
+        var eachMac = serializedMacs.split(",");
+        var macs = eachMac.map(function (serialMac) { return serialMac && MacaroonsBuilder.deserialize(serialMac); })
+
+        var rootMac = macs[0];    
+        var dischargeMac = macs[1];
+        var requestReadyMac = dischargeMac && MacaroonsBuilder.modify(rootMac).prepare_for_request(dischargeMac).getMacaroon();
+        var rootMacVerifier = new MacaroonsVerifier(rootMac);
+        rootMacVerifier = rootMacVerifier.satisfyExact("cert = " + condensedCert);
+        rootMacVerifier = (requestReadyMac ? rootMacVerifier.satisfy3rdParty(requestReadyMac) : rootMacVerifier);
+
+        return rootMacVerifier.isValid(optionsObj.secret) ? next() : next(new Error("Macaroon is not valid "));
+      });
+      // MUST CALL Afterwards
+      // callback(https.createServer(options, app));
+    });
+  });
 };
--- spec/macattack-express.spec.js
+++ spec/macattack-express.spec.js
@@ -7,22 +7,82 @@ var MacAttackExpress = require("../index.js");
   describe('createMac', function () {
 
     var res = {};
-    var next = function (err) {if (err) {console.log("err.message = %j", err.message);} };
 
     var realSerializedMacaroon = "MDAxY2xvY2F0aW9uIGh0dHA6Ly9teWJhbmsvCjAwMmNpZGVudGlmaWVyIHdlIHVzZWQgb3VyIG90aGVyIHNlY3JldCBrZXkKMDAzMGNpZCB0aGlzIHdhcyBob3cgd2UgcmVtaW5kIGF1dGggb2Yga2V5L3ByZWQKMDA1MXZpZCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD507iE_6VzIomYm5kmCri1MltQwo9pu7qJDiTzbWe-SdASonJMvEXezxP8mXr87hcKMDAxYmNsIGh0dHA6Ly9hdXRoLm15YmFuay8KMDAyZnNpZ25hdHVyZSCVAH8ifGHul9vpp280QK4x0_fJJRO61D-V8_-ip6mLhQo,MDAyMWxvY2F0aW9uIGh0dHA6Ly9hdXRoLm15YmFuay8KMDAzN2lkZW50aWZpZXIgdGhpcyB3YXMgaG93IHdlIHJlbWluZCBhdXRoIG9mIGtleS9wcmVkCjAwMmZzaWduYXR1cmUgvDOvYuPCfD-hWsrDQUl4wMHOEGw2Hp8wKRm7n_gATJgK";
 
-    it('should should run', function (){
-      var macattackFn = MacAttackExpress({secret: "secret"});
-      var req = {headers: {authorization: ""}};
+    var realCertificate = 
+      "-----BEGIN CERTIFICATE-----\n" + 
+      "MIICpDCCAYwCCQCwh3WFyZu5YDANBgkqhkiG9w0BAQsFADAUMRIwEAYDVQQDEwls\n" + 
+      "b2NhbGhvc3QwHhcNMTUxMTA5MTgzOTUxWhcNMTUxMTEwMTgzOTUxWjAUMRIwEAYD\n" + 
+      "VQQDEwlsb2NhbGhvc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCc\n" +
+      "CaQcJgQix0v49zjo3qBheRRplJG0QgzmYaYAJPA+VVJSw3nzfgKAlI8YlTDpVAOu\n" +
+      "kOaGRLwr2yuc2yguYO8efaXVxgcGPuOQ0ueFcMxhcS8rXVLsfLQdBdN7XQ4KGg2A\n" +
+      "yOwnwFcOF/L1FKY2arB7AcCVdCBm5KvOQa4VNcANDOF9c8Z/tJY5Sri1HpELL2LA\n" +
+      "sHlfrdL1IlNt6TV78UrYNET6VKoxTcjQPvSXo/NUqnByOKELCMQjM07O6fVfhFBT\n" +
+      "DFv3iiPHbT2GhIEGGRwv3wiQan5MlG1VixUshxaIxwFm6xakyW39S7grFv7N7yBy\n" +
+      "aOaKbl4+kAW1C7YhMiINAgMBAAEwDQYJKoZIhvcNAQELBQADggEBADU2UIX95Nbk\n" +
+      "WrCJjTnjUBOlMPCUYIxKZk6ocJFT6ad3Um4eBcejOg7aj+m3h62dNi6w6LxCFaXN\n" +
+      "BGLmgvLV1CQclBjK6vWrBfjoq/qFKlcRf+GGyREd/EzoyXq33ssBQAYGRQBlv/4R\n" +
+      "+BGkjRsZ+5iJAS6RnGrZRHXPORwqioHjfaZwMJc5xjvR9NhQlWhcSMg2naNQhgNA\n" + 
+      "rmqanXRKdCIeuZNxokCTi+paNvSFdqKUOPqipX1C9CpthzIZg9JOm1KWFEOgtTh3\n" + 
+      "HuLknwXj/UlJnT1XpewFUI/34jfaX1+x11dxm9Xf+cJ03mtNLbGTTVHYp//8OL7/\n" + 
+      "2l+hP7wECNo=\n" +
+      "-----END CERTIFICATE-----";
 
-      macattackFn(req, res, next);
+    function getBlankRequest(returnCert) {
+      return {
+        headers: {
+          authorization: ""}, 
+          connection : {
+            getPeerCertificate: function () {return {raw: returnCert}}}};
+
+    }
+
+    describe("running middleware", function () {
+      var middlewareFn = null;
+
+      beforeEach(function(done) {
+        MacAttackExpress({secret: "secret", hostPort: 443, hostIp: "127.0.0.1", cert: realCertificate}, function (err, middlewareFnObj) {
+          if(err) { return fail(err); }
+          middlewareFn = middlewareFnObj;
+          done();
+        });
+      });
+
+      describe('running with no macaroon', function (){
+        var err = null;
+
+        beforeEach(function(done) {
+          var req = getBlankRequest(realCertificate);
+          middlewareFn(req, res, function (errObj) {
+            err = errObj;
+            done();
+          });
+        });
+
+        it("should display 'macaroon not found'", function (){
+          expect(err.message).toEqual("macaroon not found");
+        });
+      });
+
+      describe('running with invalid macaroon', function (){
+        var err = null;
+
+        beforeEach(function(done) {
+          var req = getBlankRequest(realCertificate);
+          req.headers.authorization = "Bearer " + realSerializedMacaroon;
+          middlewareFn(req, res, function (errObj) {
+            err = errObj;
+            done();
+          });
+        });
+
+        it("should display 'Macaroon is not valid'", function (){
+          expect(err.message).toEqual("Macaroon is not valid ");
+        });
+      });
     });
 
-    it('should should run with macaroon', function (){
-      var macattackFn = MacAttackExpress({secret: "this is a different super-secret key; never use the same secret twice"});
-      var req = {headers: {authorization: "Bearer " + realSerializedMacaroon}};
-
-      macattackFn(req, res, next);
-    });
+    //NOTE FOR WORKING CASE NEED DISCHAREGE MAC... will write later
   });
 })();
